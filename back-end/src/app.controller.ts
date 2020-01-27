f8402c886a67205a79de91357aa65b523623e669
Yassine El Ouni <yassine.ouni001@gmail.com>
2020-01-27 14:23:41 +0100

    backend nestjs

---
+++ back-end/src/app.controller.ts
@@ -0,0 +1,29 @@
+import { AppService } from './app.service';
+import { Controller, Request, UseGuards, Post, Get, Delete , Put } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+}
